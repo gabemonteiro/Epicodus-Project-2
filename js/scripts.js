2c36bd839ce4dcd338ee42b91e42598da11c1218
unknown <gabe.c.monteiro@gmail.com>
2019-09-19 12:24:15 -0700

    add new formatting / beginning some javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     event.preventDefault();
     var name = $("input#name").val();
 
+    var age = document.getElementById('ageRange')
+    var interests = document.getElementById('interests')
+    var age = document.getElementById('ageRange')
+
     $(".name").text(name);
 
     $("#result").show();
