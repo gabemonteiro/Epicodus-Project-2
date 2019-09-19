6e2b7da0622c104e62d7edebbef014dbf01fdff7
unknown <gabe.c.monteiro@gmail.com>
2019-09-19 12:04:57 -0700

    add minor adjustments to format as well as basic results

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
   $("form#quiz").submit(function() {
     event.preventDefault();
+    var name = $("input#name").val();
+
+    $(".name").text(name);
+
+    $("#result").show();
