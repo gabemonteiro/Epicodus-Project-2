feb7dd4b1346653ff6f1bb7ff65e78d1075d0e3c
unknown <gabe.c.monteiro@gmail.com>
2019-09-22 19:52:54 -0700

    add beginning of js functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,27 @@ $(document).ready(function() {
     var interests = $('#interests').val();
     var color = $('#color-select').val();
 
+    // Check to make sure user has made all selections
+    if (name === '' || age === 'unselected' || interests === 'unselected' || color === 'unselected') {
+      alert("Please fill out all your selections")
+      event.preventDefault()
+      return;
+    }
+
+    // Logic to handle which results we display
+    if (age === 'over50') {
+      $("#old").toggleClass('hidden');
+      $("#old .name").text(name);
+      $("#quiz").hide();
+      
+    }
+    if (age === "sub18" && (interests === "web" || "mobile" || "apps" || "undecided")) {
+      $("#resultsYoung").toggleClass("hidden")
+      $('#resultsYoung h1').toggleClass(color);
+      $("#resultsYoung .name").text(name);
+      $("#quiz").hide();
+    }
+
     event.preventDefault()
   });
 });
